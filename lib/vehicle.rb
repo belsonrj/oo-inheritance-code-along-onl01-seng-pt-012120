19afe0e5fdd520d3e6ebecfd513876393d4d7558
Rick Belson <belsonrj@aol.com>
2020-02-20 03:08:08 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,12 @@
 class Vehicle
-
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(whee_size, wheel_size)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end 
+  
+  def go 
+    "v"
+  
 end
